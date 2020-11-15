62412386bb4fe5013db3ccfd9ade4c8fa6d395e1
Maycom Willams <74081416+Maycomwill@users.noreply.github.com>
2020-11-14 22:17:44 -0300

    Troca de sistema de posição
    Troca de position absolute para flexbox

--- java.js
+++ java.js
@@ -1,6 +1,7 @@
 var s1;
 
 function clicar() {
+
   var tn1 = window.document.getElementById("peso");
   var tn2 = window.document.getElementById("altura");
   var res = window.document.querySelector("div.resultado");
@@ -11,23 +12,24 @@ function clicar() {
 }
 
 function cor() {
+  var body = window.document.getElementById("fundo")
   var res = window.document.querySelector("div.resultado");
   if (s1 < 16) {
-    res.style.background = "#0000FF";
+    body.style.background = "#0000FF";
   } else if (s1 >= 16 && s1 <= 16.99) {
-    res.style.background = "#0066FF";
+    body.style.background = "#0066FF";
   } else if (s1 >= 17 && s1 <= 18.4) {
-    res.style.background = "#00CC99";
+    body.style.background = "#00CC99";
   } else if (s1 >= 18.5 && s1 <= 24.99) {
-    res.style.background = "#00FF00";
+    body.style.background = "#00FF00";
   } else if (s1 >= 25 && s1 <= 29.99) {
-    res.style.background = "#FFCC00";
+    body.style.background = "#FFCC00";
   } else if (s1 >= 30 && s1 <= 34.99) {
-    res.style.background = "#FF6600";
+    body.style.background = "#FF6600";
   } else if (s1 >= 35 && s1 <= 39.99) {
-    res.style.background = "#FF3300";
+    body.style.background = "#FF3300";
   } else if (s1 > 40) {
-    res.style.background = "#FF0000";
+    body.style.background = "#FF0000";
   } else {
   }
 }
